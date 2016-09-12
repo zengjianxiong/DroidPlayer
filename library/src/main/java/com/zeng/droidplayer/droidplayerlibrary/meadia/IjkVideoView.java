17235805f1c02a67fba46df029e97e3b91228f88
zengjianxiong <752506363@qq.com>
2016-09-12 18:06:47 +0800

    添加 IjkVideoView

---
+++ library/src/main/java/com/zeng/droidplayer/droidplayerlibrary/meadia/IjkVideoView.java
@@ -0,0 +1,85 @@
+package com.zeng.droidplayer.droidplayerlibrary.meadia;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+import android.widget.MediaController;
+
+/**
+ * Created by gskl on 16/9/12.
+ */
+public class IjkVideoView extends FrameLayout implements MediaController.MediaPlayerControl {
+    public IjkVideoView(Context context) {
+        super(context);
+    }
+
+    public IjkVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public IjkVideoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public IjkVideoView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public int getDuration() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return 0;
+    }
+
+    @Override
+    public void seekTo(int i) {
+
+    }
+
+    @Override
+    public boolean isPlaying() {
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return 0;
+    }
+
+    @Override
+    public boolean canPause() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
+    }
+}
